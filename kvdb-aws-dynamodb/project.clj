65b4c80791a71b8652717e56280305c3807204e2
Biiwide <tcushman@gmail.com>
2024-06-10 10:10:08 -0500

    Updating dependencies in kvdb-aws-dynamodb

--- kvdb-aws-dynamodb/project.clj
+++ kvdb-aws-dynamodb/project.clj
@@ -12,18 +12,20 @@
   :plugins [[biiwide/copy-deps "0.7.1"]
             [lein-ancient "1.0.0-RC3"]]
 
-  :dependencies [[amazonica "0.3.161"
+  :dependencies [[amazonica "0.3.166"
                   :exclusions [com.amazonaws/aws-java-sdk
                                com.amazonaws/amazon-kinesis-client
-                               com.amazonaws/dynamodb-streams-kinesis-adapter]]
+                               com.amazonaws/dynamodb-streams-kinesis-adapter
+                               com.taoensso/nippy]]
                  [biiwide/kvdb-api "0.1.2"]
+                 [com.amazonaws/aws-java-sdk-dynamodb "1.12.739"]
                  [org.clojure/clojure "1.10.3" :scope "provided"]]
 
   :repl-options {:init-ns biiwide.kvdb.aws.dynamodb}
 
   :profiles
   {:dev
-   {:dependencies [[com.amazonaws/DynamoDBLocal "1.19.0"]
+   {:dependencies [[com.amazonaws/DynamoDBLocal "1.20.0"]
                    [org.clojure/test.check "1.1.1"]]
 
     :prep-tasks   ["javac" "compile" "copy-deps"]
@@ -31,11 +33,12 @@
     :copy-deps    {:dependencies [[com.almworks.sqlite4java/libsqlite4java-linux-amd64 "1.0.392" :extension "so"]
                                   [com.almworks.sqlite4java/libsqlite4java-linux-i386 "1.0.392" :extension "so"]
                                   [com.almworks.sqlite4java/libsqlite4java-osx "1.0.392" :extension "dylib"]
+                                  [io.github.ganadist.sqlite4java/libsqlite4java-osx-aarch64 "1.0.392" :extension "dylib"]
                                   [com.almworks.sqlite4java/sqlite4java-win32-x64 "1.0.392" :extension "dll"]
                                   [com.almworks.sqlite4java/sqlite4java-win32-x86 "1.0.392" :extension "dll"]]
                    :destination  "native-libs"}
 
-    :jvm-opts     ["-Djava.library.path=native-libs/com.almworks.sqlite4java"]
+    :jvm-opts     ["-Djava.library.path=native-libs/com.almworks.sqlite4java:native-libs/io.github.ganadist.sqlite4java"]
     }
    }
   )
